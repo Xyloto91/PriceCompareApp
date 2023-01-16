0515cff2bc430593ee3d4c72bbe2ee6f8c816e41
vjozic91 <vjozic@fesb.hr>
2023-01-16 21:43:06 +0100

    fix(refactoring): refactoring factory method pattern

--- PriceCompareApp.Core/WebScraper.cs
+++ PriceCompareApp.Core/WebScraper.cs
@@ -10,29 +10,7 @@ namespace PriceCompareApp.Core
 {
     public class WebScraper
     {
-        private readonly Dictionary<WebSite, WebScraperFactory> _factories;
-
-        public WebScraper()
-        {
-            _factories = new Dictionary<WebSite, WebScraperFactory>();
-
-            foreach (WebSite webSite in Enum.GetValues(typeof(WebSite)))
-            {
-                _factories.Add(
-                    webSite,
-                    (WebScraperFactory)
-                        Activator.CreateInstance(
-                            Type.GetType(
-                                $"PriceCompareApp.Core.Factories.{webSite}WebScraperFactory, PriceCompareApp.Core"
-                            )
-                        )
-                );
-            }
-        }
-
-        public static WebScraper InitializeWebScraper => new WebScraper();
-
-        public async Task<List<Item>> Execute(WebSite website, List<string> itemCodes) =>
-            await _factories[website].Create().RunScrapingAsync(itemCodes);
+        public async Task<List<Item>> Execute(WebSite webSite, List<string> itemCodes) =>
+            await new WebScraperFactory().Create(webSite).RunScrapingAsync(itemCodes);
     }
 }
---
+++ PriceCompareApp.Core/WebScraperFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using PriceCompareApp.Core.Scrapers;
+using PriceCompareApp.Model;
+using static PriceCompareApp.Common.Helper;
+
+namespace PriceCompareApp.Core
+{
+    public class WebScraperFactory
+    {
+        public IWebScraper Create(WebSite website)
+        {
+            switch (website)
+            {
+                case WebSite.Dekom:
+                    return new DekomWebScraper();
+                case WebSite.Eltom:
+                    return new EltomWebScraper();
+                case WebSite.Elkond:
+                    return new ElkondWebScraper();
+                case WebSite.Loren:
+                    return new LorenWebScraper();
+                case WebSite.Vrecool:
+                    return new VrecoolWebScraper();
+                case WebSite.StatusFrigo:
+                    return new StatusFrigoWebScraper();
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
